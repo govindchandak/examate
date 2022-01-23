8af0ba2977b4ec76744e0fff7ecc603fa28f16ea
healthassistor <govindchandak04@gail.com>
2022-01-23 03:51:32 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("welocme to examate"),
+          ),
+        ),
+      ),
+    );
+  }
+}
